c979fa5f7f34ce1aa1c6d2b00ca5a36124560b03
ohager <oliver.hager@dextra-sw.com>
2021-10-17 11:36:35 -0300

    fix: some sonar issues

--- packages/crypto/src/passPhraseGenerator.ts
+++ packages/crypto/src/passPhraseGenerator.ts
@@ -2,12 +2,15 @@
 /**
  * Original work Copyright (c) 2018 PoC-Consortium
  * Modified work Copyright (c) 2019 Burst Apps Team
+ * Modified work Copyright (c) 2021 Signum Network
  */
 
 import * as _seedrandom from 'seedrandom';
-import { words } from './words';
+import {words} from './words';
 
 const seedrandom = (_seedrandom as any).default || _seedrandom;
+
+// TODO: review this entire class potentially refactor.
 
 /**
  * A secure random passphrase generator
@@ -31,26 +34,22 @@ export class PassPhraseGenerator {
      * Generates a passphrase based on priory set
      * @return The _secret_ twelve-worded passphrase
      */
-	public generate(): string[] {
-        // seed with given seed if seed was given, yep
-        this.seed.map(element => seedrandom(element, { "entropy": true, "global":true }));
-
-        // get random words
+    public generate(): string[] {
+        this.seed.forEach(element => seedrandom(element, {'entropy': true, 'global': true}));
         let randomWords: string[] = [];
-		for (let i = 0; i < 12; i++) {
-			let number = Math.floor((Math.random() * PassPhraseGenerator.wordCount) + 1);
-			randomWords.push(words[number]);
-		}
-        // return concatenated string
+        for (let i = 0; i < 12; i++) {
+            let number = Math.floor((Math.random() * PassPhraseGenerator.wordCount) + 1);
+            randomWords.push(words[number]);
+        }
         return randomWords;
-	}
+    }
 
     /**
      * Sets a new seed for generation
      * @param seed
      */
-	public reSeed(seed) {
-		this.seed = seed;
+    public reSeed(seed) {
+        this.seed = seed;
     }
 
     /**
