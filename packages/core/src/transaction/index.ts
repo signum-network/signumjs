f1d0165649597412cf77ebb84a0e834d7893ae43
ohager <oliver.hager@dextra-sw.com>
2021-11-16 10:47:46 -0300

    fix: unit tests

--- packages/core/src/transaction/index.ts
+++ packages/core/src/transaction/index.ts
@@ -2,6 +2,6 @@ export * from './isMultiOutSameTransaction';
 export * from './isMultiOutTransaction';
 export * from './getRecipientAmountsFromMultiOutPayment';
 export * from './getRecipientsAmount';
-export * from './parseTransactionBytes/parseTransactionBytes';
-export * from './verifyUnsignedTransaction';
+export * from './verify/parseTransactionBytes';
+export * from './verify/verifyUnsignedTransaction';
 
