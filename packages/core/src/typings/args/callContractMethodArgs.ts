776d046ebc2052a138a0f5d6d5df99df792ef4f5
ohager <oliver@devbutze.com>
2022-02-15 11:47:34 -0300

    fix: adjust callContractMethod arg interface

--- packages/core/src/typings/args/callContractMethodArgs.ts
+++ packages/core/src/typings/args/callContractMethodArgs.ts
@@ -1,17 +1,14 @@
 import {ContractData} from '@signumjs/contracts';
+import {DefaultSendArgs} from './defaultSendArgs';
 
 /**
  * The argument object for [[ContractApi.callContractMethod]]
  *
  * @module core
  */
-export interface CallContractMethodArgs {
+export interface CallContractMethodArgs extends DefaultSendArgs {
     amountPlanck: string;
     contractId: string;
-    deadline?: number;
-    feePlanck: string;
     methodArgs?: ContractData[];
     methodHash: string;
-    senderPrivateKey: string;
-    senderPublicKey: string;
 }
