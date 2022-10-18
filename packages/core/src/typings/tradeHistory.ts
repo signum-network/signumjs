9d2cad6e49242b9f8c8dda04b9bacb8dbf25d48a
ohager <oliver@devbutze.com>
2022-10-18 17:08:08 -0300

    fix: added timestamp to journal entry

--- packages/core/src/typings/tradeHistory.ts
+++ packages/core/src/typings/tradeHistory.ts
@@ -16,6 +16,7 @@ export interface JournalEntry {
     quantityQNT: string;
     priceNQT: string;
     height: number;
+    timestamp: number;
     name: string;
     decimals: number;
     price: string;
