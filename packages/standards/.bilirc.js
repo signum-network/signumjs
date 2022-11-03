cc2dfb70b908d28c86fd393c038ddb958da39561
ohager <oliver@devbutze.com>
2022-11-03 10:48:17 -0300

    wip: added json schema for src44

---
+++ packages/standards/.bilirc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  input: "./src/index.ts",
+  output: {
+    moduleName: "sig$standards",
+    fileName: "signumjs.standards[min].js",
+    format: ["umd", "umd-min"],
+    dir: "./dist"
+  },
+  env: {
+    NODE_ENV: "production"
+  },
+  plugins: {
+    ['node-resolve'] : {
+      jsnext: true,
+      preferBuiltins: true,
+      browser:true
+    }
+  }
+};
---
+++ packages/standards/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * The Standards Module implements all [Signum SRCs](https://github.com/signum-network/SIPs/tree/master/SRC)
+ *
+ * Still WORK IN PROGRESS
+ *
+ * @moduledefinition standards
+ */
+
+export {
+};
+
