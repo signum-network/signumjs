857ca06a48de8bd9b2420223d8621598a35edbd0
ohager <oliver@devbutze.com>
2023-03-02 07:40:58 -0300

    fix: fixes #41 - arguments for subscriptions

--- packages/core/src/typings/args/cancelSubscriptionArgs.ts
+++ packages/core/src/typings/args/cancelSubscriptionArgs.ts
@@ -1,23 +1,12 @@
-import {Attachment} from '../attachment';
+import {DefaultSendArgs} from './defaultSendArgs';
 
 /**
  * The argument object for [[TransactionApi.cancelSubscription]]
- *
- * @param subscriptionId The subscription to be cancelled
- * @param feePlanck The fee as Planck value
- * @param senderPublicKey The senders public key for sending an _unsigned_ message
- * @param senderPrivateKey The senders private key to _sign_ the message
- * @param attachment An _optional_ attachment
- * @param deadline The _optional_ transactions deadline in minutes until it's being removed from mempool.
- * This may happen on low fees. Defaults to 1440 (maximum)
- *
  * @module core
  */
-export interface CancelSubscriptionArgs {
+export interface CancelSubscriptionArgs extends DefaultSendArgs {
+    /**
+     The subscription to be cancelled
+     */
     subscriptionId: string;
-    feePlanck: string;
-    senderPublicKey: string;
-    senderPrivateKey: string;
-    attachment?: Attachment;
-    deadline?: number;
 }
