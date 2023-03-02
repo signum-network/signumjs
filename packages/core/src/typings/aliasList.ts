ed7d56364494065c16c28bce8d55079991e06b39
ohager <oliver@devbutze.com>
2023-03-02 07:58:46 -0300

    fix: fixed #38 - actually, was there already, but added the nextIndex stuff

--- packages/core/src/typings/aliasList.ts
+++ packages/core/src/typings/aliasList.ts
@@ -2,13 +2,14 @@
  * Original work Copyright (c) 2019 Burst Apps Team
  */
 import {Alias} from './alias';
+import {Indexable} from './indexable';
 
 /**
  * Alias List
  *
  * @module core
  */
-export interface AliasList {
+export interface AliasList extends Indexable{
     aliases: Alias[];
     requestProcessingTime: number;
 }
