ec1bf44925551e71ada547e2e9d13bb34f3622cb
ohager <oliver@devbutze.com>
2024-12-28 06:48:09 -0300

    chore: fixing outdated packages

--- generate-coverage-report.sh
+++ generate-coverage-report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#rm -rf .nyc_output
+rm -rf .nyc_output
 mkdir -p .nyc_output
 find packages/*/coverage -name '*.json' -exec cp {} .nyc_output/ \;
 nyc merge .nyc_output coverage.json
