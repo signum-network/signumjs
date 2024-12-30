bf9c7a39e8d23e29ada60ec76d29d6e3c135e153
ohager <oliver@devbutze.com>
2024-12-30 11:30:29 -0300

    chore: renaming

--- packages/contracts/src/__tests__/generateMethodCall.spec.ts
+++ packages/contracts/src/__tests__/generateMethodCall.spec.ts
@@ -1,18 +1,18 @@
 import {generateMethodCall} from '../generateMethodCall';
 
-const ContractMethodHash = '-327803124352370';
+const ContractMethodId = '-327803124352370';
 
 describe('generateMethodCall', () => {
     it('generates a method call without argument', () => {
         const message = generateMethodCall({
-            methodId: ContractMethodHash
+            methodId: ContractMethodId
         });
         expect(message).toBe('8e7a3763ddd5feff');
     });
 
     it('generates a method call with three arguments', () => {
         const message = generateMethodCall({
-            methodId: ContractMethodHash,
+            methodId: ContractMethodId,
             methodArgs: [true, 1234, '-1234567890'],
         });
         expect(message).toBe('8e7a3763ddd5feff0100000000000000d2040000000000002efd69b6ffffffff');
